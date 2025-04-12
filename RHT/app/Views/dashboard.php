540e38936b1b7ae677170a337257e9c0f261b52c
GOWTHAM <rhtex>
2025-04-12 15:59:30 +0530

    Removed unwanted changes

--- RHT/app/Views/dashboard.php
+++
@@ -1,8 +0,0 @@
-<?php include 'layouts/head.php'; ?>
-<?php include 'layouts/layout-vertical.php'; ?>
-
-    
-        <h4> Welcome to Home </h4>
-    </div>
-</div>
-<?php include 'layouts/footer.php'; ?>
